067d98e954390d19381a25455354271beb6d64a8
GemelliAngelo <gemeangelo27@gmail.com>
2024-11-29 15:22:44 +0100

    add filter + map

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -7,9 +7,17 @@ export default function Main() {
       <div className="container">
         <div className="d-flex justify-content-center">
           <div className="row row-cols-2 g-3">
-            {postsData.map((post) => {
-              <Card />;
-            })}
+            {postsData
+              .filter((post) => post.published == true)
+              .map((post) => {
+                <Card
+                  title={post.title}
+                  image={post.image}
+                  description={post.content}
+                  tags={post.tags}
+                  published={post.published}
+                />;
+              })}
           </div>
         </div>
       </div>
